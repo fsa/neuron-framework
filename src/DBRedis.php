e0c8579fc69965d1bef1d2f9de6f7aa005f4cf21
Sergey Fedotov <fsa@tavda.info>
2022-03-26 19:44:16 +0700

    Больше возможностей настройки соединения с Redis.

--- src/DBRedis.php
+++ src/DBRedis.php
@@ -7,25 +7,42 @@ class DBRedis {
     private static $redis=null;
 
     private function __construct() {
-        
     }
 
     private function __clone() {
-        
     }
 
     public static function getInstance(): \Redis {
         if (self::$redis) {
             return self::$redis;
         }
-        $host=getenv('REDIS_HOST')?getenv('REDIS_HOST'):'127.0.0.1';
-        $port=getenv('REDIS_PORT')?getenv('REDIS_PORT'):6379;
+        return self::connect(getenv('REDIS_URL'));
+    }
+
+    public static function connect($url=null) {
+        if (!$url) {
+            $db = ['host'=>'127.0.0.1'];
+        }  else {
+            $db = parse_url($url);
+        }
         self::$redis=new \Redis();
-        self::$redis->connect($host, $port);
-        if(getenv('REDIS_AUTH')) {
-            if(!self::$redis->auth(getenv('REDIS_AUTH'))) {
-                throw new AppException('Redis Auth Failed');
+        try {
+            self::$redis->connect((empty($db['scheme'])?'':$db['scheme']."://").$db["host"], $db["port"] ?? 6379);
+        } catch (\RedisException $ex) {
+            throw new AppException('Redis connect failed: '.$ex->getMessage());
+        }
+        if(!empty($db["pass"])) {
+            if(empty($db["user"])) {
+                $result=self::$redis->auth($db["pass"]);
+            } else {
+                $result=self::$redis->auth([$db["user"], $db["pass"]]);
             }
+            if(!$result) {
+                throw new AppException('Redis auth failed');
+            }
+        }
+        if(!empty($db["path"])) {
+            self::$redis->select((int)ltrim($db['path'], '/'));
         }
         return self::$redis;
     }
