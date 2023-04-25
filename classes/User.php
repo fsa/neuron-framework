ef54e7c3c9829f73fc61f4bbdb54a2095cbc5073
Sergei Fedotov <fsa@tavda.info>
2023-04-26 01:15:13 +0700

    База пользователей теперь работает через обёртку PDO

--- classes/User.php
+++ classes/User.php
@@ -2,29 +2,24 @@
 
 namespace FSA\Neuron;
 
-use PDO;
 use App\Core;
+use FSA\Neuron\Database\Pgsql;
 
 class User implements UserInterface
 {
-
     public $uuid;
     public $login;
     public $name;
     public $email;
     public $scope;
-    private $pdo;
 
-    function __construct(PDO $pdo)
+    function __construct(private Database\Pgsql $pgsql)
     {
-        $this->pdo = $pdo;
     }
 
     public function login($login, $password): bool
     {
-        $s = $this->pdo->prepare('SELECT uuid, password_hash FROM users WHERE (login=? OR (email IS NOT NULL AND email=?)) AND NOT COALESCE(disabled, false)');
-        $s->execute([$login, $login]);
-        $user = $s->fetchObject();
+        $user = $this->pgsql->executeQuery('SELECT uuid, password_hash FROM users WHERE (login=? OR (email IS NOT NULL AND email=?)) AND NOT COALESCE(disabled, false)', [$login, $login])->fetchObject();
         if (!($user and password_verify($password, $user->password_hash))) {
             return false;
         }
@@ -37,9 +32,7 @@ class User implements UserInterface
         if (!isset($args['uuid'])) {
             return false;
         }
-        $s = $this->pdo->prepare("WITH usr AS (SELECT * FROM users u WHERE uuid=? AND NOT COALESCE(disabled, false)), groups_set AS (SELECT uuid, unnest(groups) AS gid FROM usr), gscope_set AS (SELECT uuid, unnest(ug.scope) AS gscope FROM groups_set g LEFT JOIN user_groups ug ON g.gid=ug.name GROUP BY uuid, gscope), gscope AS (SELECT uuid, array_agg(gscope) AS group_scope FROM gscope_set GROUP BY uuid) SELECT json_build_object('uuid',uuid, 'login',login, 'name', name, 'email', email, 'scope', to_json(group_scope||scope)) FROM usr LEFT JOIN gscope USING (uuid)");
-        $s->execute([$args['uuid']]);
-        $entity = $s->fetchColumn();
+        $entity = $this->pgsql->executeQuery("WITH usr AS (SELECT * FROM users u WHERE uuid=? AND NOT COALESCE(disabled, false)), groups_set AS (SELECT uuid, unnest(groups) AS gid FROM usr), gscope_set AS (SELECT uuid, unnest(ug.scope) AS gscope FROM groups_set g LEFT JOIN user_groups ug ON g.gid=ug.name GROUP BY uuid, gscope), gscope AS (SELECT uuid, array_agg(gscope) AS group_scope FROM gscope_set GROUP BY uuid) SELECT json_build_object('uuid',uuid, 'login',login, 'name', name, 'email', email, 'scope', to_json(group_scope||scope)) FROM usr LEFT JOIN gscope USING (uuid)", [$args['uuid']])->fetchColumn();
         if (!$entity) {
             return false;
         }
@@ -81,7 +74,7 @@ class User implements UserInterface
 
     public static function validate(array $properties): self
     {
-        $user = new self(Core::sql());
+        $user = new self(Core::container()->get(Pgsql::class));
         return $user->refresh($properties) ? $user : null;
     }
 }
